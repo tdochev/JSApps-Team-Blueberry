a8ff0cda346b95301e5d9da570cc3103983bbe7c
Tsvetomir Dochev <tddochev@gmail.com>
2017-04-22 21:14:47 +0300

    added connection to mongoDB

--- app.js
+++ app.js
@@ -2,10 +2,32 @@
 'use strict';
 
 let express = require('express');
-var app = express();
+let MongoClient = require('mongodb').MongoClient;
+
+let app = express();
 
 app.use(express.static('public'));
 
 const port = process.env.PORT || 3001;
+const dbURI = process.env.MONGODB || '';
+
+// Connect to the db
+
+
+
+app.get('/api/users', function(req, res) {
+    MongoClient.connect('dbURI', function(err, db) {
+        if (!err) {
+            console.log('Connected to MLab!');
+        }
+        let temp = db.collection('users').find({}).toArray(function(err, docs) {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify(docs));
+            db.close();
+        });
+    });
+});
+
+
 
 app.listen(port, () => console.log('Magic happens at port' + port));
