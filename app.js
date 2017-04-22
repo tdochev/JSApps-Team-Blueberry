4358e8caafbd5c28e697c57f46c08fe708cc9ab7
Tsvetomir Dochev <tddochev@gmail.com>
2017-04-22 23:53:37 +0300

    ?

--- app.js
+++ app.js
@@ -13,11 +13,12 @@ const port = process.env.PORT || 3001;
 
 app.get('/api/users', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(db.getAllUsers));
+    res.send(JSON.stringify(db.getAllUsers()));
 });
 
 app.post('/api/users', (req, res) => {
     let user = req.body;
+    console.log(user);
     if (!user || typeof user.username !== 'string') {
         res.status(400)
             .json('Invalid user');
