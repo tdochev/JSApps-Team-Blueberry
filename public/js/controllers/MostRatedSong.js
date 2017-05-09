9229417d1e58c6afe3ac01d70437be63990f8f9f
SvetlomirBalevski <s.balevski@gmail.com>
2017-05-09 10:59:35 +0300

    restored html and css

---
+++ public/js/controllers/MostRatedSong.js
@@ -0,0 +1,27 @@
+/* globals $, localStorage */
+
+import Requester from 'requester';
+import HandlebarsTemplate from 'templates';
+import toastr from 'toastr';
+
+var requester = new Requester();
+
+export default class MostRatedSong {
+    bestDSon() {
+        var options = {
+            headers: {
+                'userAuthKey': localStorage.getItem('userAuthKey')
+            }
+        };
+
+        requester.get('/api/songs', options).then(function(resp) {
+            console.log(resp);
+            var template = new HandlebarsTemplate();
+            template.loadTemplate('songs').then(function(template) {
+                $('#main-container').append(template(resp));
+            });
+        }).catch(function err() {
+            console.log(err);
+        });
+    }
+}
